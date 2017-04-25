e1d7d14754a9c9cd1d3e007ed632b9fdf5fee056
15039836 <15039836@myrp.edu.sg>
2017-04-25 10:46:07 +0800

    weird ._.

--- app/src/main/java/p01_dailygoals/android/myapplicationdev/com/demoimageview/MainActivity.java
+++ app/src/main/java/p01_dailygoals/android/myapplicationdev/com/demoimageview/MainActivity.java
@@ -2,12 +2,21 @@ package p01_dailygoals.android.myapplicationdev.com.demoimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare the ImageView object called ivDay2
+    ImageView ivDay2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day2);
+
     }
 }
